e1a932848f0f41787d769770eaa8074e7a8a2f80
Abner Castro <abnerfcastro@gmail.com>
2020-12-27 14:34:41 -0300

    Change App to a class component

--- src/App.js
+++ src/App.js
@@ -1,36 +1,49 @@
+import { Component } from "react"
+
 import Navbar from "./Navbar"
 import Card from "./Card"
+
+import shuffle from "shuffle-array"
+
 import "./App.css"
 
-function App() {
-  const cardsArray = [
-    { id: 0, backgroundColor: "red" },
-    { id: 1, backgroundColor: "red" },
-    { id: 2, backgroundColor: "navy" },
-    { id: 3, backgroundColor: "navy" },
-    { id: 4, backgroundColor: "green" },
-    { id: 5, backgroundColor: "green" },
-    { id: 6, backgroundColor: "yellow" },
-    { id: 7, backgroundColor: "yellow" },
-    { id: 8, backgroundColor: "black" },
-    { id: 9, backgroundColor: "black" },
-    { id: 10, backgroundColor: "purple" },
-    { id: 11, backgroundColor: "purple" },
-    { id: 12, backgroundColor: "pink" },
-    { id: 13, backgroundColor: "pink" },
-    { id: 14, backgroundColor: "lightskyblue" },
-    { id: 15, backgroundColor: "lightskyblue" },
-  ]
+export default class App extends Component {
+  constructor(props) {
+    super(props)
 
-  const cards = cardsArray.map((c) => (
-    <Card backgroundColor={c.backgroundColor} />
-  ))
-  return (
-    <div className="App">
-      <Navbar />
-      {cards}
-    </div>
-  )
+    let cards = [
+      { id: 0, backgroundColor: "red" },
+      { id: 1, backgroundColor: "red" },
+      { id: 2, backgroundColor: "navy" },
+      { id: 3, backgroundColor: "navy" },
+      { id: 4, backgroundColor: "green" },
+      { id: 5, backgroundColor: "green" },
+      { id: 6, backgroundColor: "yellow" },
+      { id: 7, backgroundColor: "yellow" },
+      { id: 8, backgroundColor: "black" },
+      { id: 9, backgroundColor: "black" },
+      { id: 10, backgroundColor: "purple" },
+      { id: 11, backgroundColor: "purple" },
+      { id: 12, backgroundColor: "pink" },
+      { id: 13, backgroundColor: "pink" },
+      { id: 14, backgroundColor: "lightskyblue" },
+      { id: 15, backgroundColor: "lightskyblue" },
+    ]
+
+    cards = shuffle(cards)
+
+    this.state = { cards }
+  }
+
+  render() {
+    const cards = this.state.cards.map((c) => (
+      <Card key={c.id} backgroundColor={c.backgroundColor} />
+    ))
+    return (
+      <div className="App">
+        <Navbar />
+        {cards}
+      </div>
+    )
+  }
 }
-
-export default App
