5c4073a099d32abd3a6110a074eac50081f9a84c
Abner Castro <abnerfcastro@gmail.com>
2020-12-27 19:22:08 -0300

    Refactor code for final adjustments

--- src/App.js
+++ src/App.js
@@ -1,21 +1,21 @@
-import { Component } from "react";
+import { Component } from "react"
 
-import Navbar from "./Navbar";
-import Card from "./Card";
+import Navbar from "./Navbar"
+import Card from "./Card"
 
-import shuffle from "shuffle-array";
+import shuffle from "shuffle-array"
 
-import "./App.css";
+import "./App.css"
 
 const CardState = {
   HIDING: 0,
   SHOWING: 1,
   MATCHING: 2,
-};
+}
 
 export default class App extends Component {
   constructor(props) {
-    super(props);
+    super(props)
 
     let cards = [
       { id: 0, cardState: CardState.HIDING, backgroundColor: "red" },
@@ -34,33 +34,45 @@ export default class App extends Component {
       { id: 13, cardState: CardState.HIDING, backgroundColor: "pink" },
       { id: 14, cardState: CardState.HIDING, backgroundColor: "lightskyblue" },
       { id: 15, cardState: CardState.HIDING, backgroundColor: "lightskyblue" },
-    ];
+    ]
 
-    cards = shuffle(cards);
+    cards = shuffle(cards)
 
-    this.state = { cards, secondClick: false };
+    this.state = { cards, secondClick: false, canClick: true }
+
+    this.handleNewGame = this.handleNewGame.bind(this)
   }
 
   handleCardClick(id) {
-    const cards = [...this.state.cards];
-    const card = cards.find((c) => c.id === id);
-    if (!card) return;
-    card.cardState = CardState.SHOWING;
+    if (this.state.canClick === false) return
+
+    const cards = [...this.state.cards]
+    const card = cards.find((c) => c.id === id)
+
+    if (!card || card.cardState === CardState.SHOWING) return
+
+    card.cardState = CardState.SHOWING
 
     if (this.state.secondClick) {
-      setTimeout(() => {
-        const selectedBackgroundColor = card.backgroundColor;
-        const matchingCandidates = cards.filter((c) => c.cardState === CardState.SHOWING);
-        const isMatch = matchingCandidates.every((e) => e.backgroundColor === selectedBackgroundColor);
-        matchingCandidates.forEach((elem) => (elem.cardState = isMatch ? CardState.MATCHING : CardState.HIDING));
-        this.setState({ cards });
-      }, 1000);
-
-      this.setState({ cards, secondClick: false });
-      return;
+      this.setState({ cards, secondClick: false, canClick: false }, () => {
+        setTimeout(() => {
+          const selectedBackgroundColor = card.backgroundColor
+          const matchingCandidates = cards.filter((c) => c.cardState === CardState.SHOWING)
+          const isMatch = matchingCandidates.every((e) => e.backgroundColor === selectedBackgroundColor)
+          matchingCandidates.forEach((elem) => (elem.cardState = isMatch ? CardState.MATCHING : CardState.HIDING))
+          this.setState({ cards, canClick: true })
+        }, 1000)
+      })
+      return
     }
 
-    this.setState({ cards, secondClick: true });
+    this.setState({ cards, secondClick: true })
+  }
+
+  handleNewGame() {
+    let cards = this.state.cards.map((card) => ({ ...card, cardState: CardState.HIDING }))
+    cards = shuffle(cards)
+    this.setState({ cards, secondClick: false, canClick: true })
   }
 
   render() {
@@ -71,12 +83,12 @@ export default class App extends Component {
         backgroundColor={c.backgroundColor}
         onClick={() => this.handleCardClick(c.id)}
       />
-    ));
+    ))
     return (
       <div className="App">
-        <Navbar />
+        <Navbar onNewGame={this.handleNewGame} isLocked={this.state.canClick === false} />
         {cards}
       </div>
-    );
+    )
   }
 }
