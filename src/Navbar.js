1f165166fe6f9ab2e5c67b2570b006c0d970d99a
Abner Castro <abnerfcastro@gmail.com>
2020-12-27 19:21:43 -0300

    Add UI visual for when click control is locked

--- src/Navbar.js
+++ src/Navbar.js
@@ -1,11 +1,17 @@
 import "./Navbar.css"
 
-const Navbar = () => {
+const Navbar = ({ onNewGame, isLocked }) => {
   return (
     <header>
       <h2>
         <a href="/">Memory Game</a>
       </h2>
+      <nav>
+        {isLocked ? <li style={{ color: "red" }}>LOCKED</li> : null}
+        <li>
+          <button onClick={onNewGame}>New Game</button>
+        </li>
+      </nav>
     </header>
   )
 }
